818f0a3860b2bdcb7028cd53c412d0967e123939
wildyproject <84273222+wildyproject@users.noreply.github.com>
2021-05-16 18:57:42 +0700

    Create autostart.sh

---
+++ autostart.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Auto Setup SSH & OpenVPN Tunneling
+# Supported For Centos 7 , 8 & Fedora 31 , 32 , 33
+# Auther : Wildy Project ( Wildy Sheverando )
+# ======================================================
+
+# Installation
