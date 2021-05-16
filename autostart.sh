928a4efea954cbcd4ba0220d1d6aef0c7c917629
wildyproject <84273222+wildyproject@users.noreply.github.com>
2021-05-16 19:24:43 +0700

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -5,6 +5,29 @@
 # License : ©
 # ======================================================
 
+# Root Validating
+if [ "${EUID}" -ne 0 ]; then
+		echo -e "${RED}Eror !${NC} Please Run This Script As Root User !!"
+		exit 0
+fi
+
+# Checking Operating System Information
+OS_MU_LAPET=$( cat /etc/os-release | grep ID | cut -d " " -f 2-3 | head -n1 | sed 's/ID=//g' )
+if [[ $OS_MU_LAPET == "centos" ]]; then
+  echo "$OS_MU_LAPET Detected !"
+elif [[ $OS_MU_LAPET == "fedora" ]]; then
+  echo "$OS_MU_LAPET Detected !"
+elif [[ $OS_MU_LAPET == "debian" ]]; then
+  echo "$OS_MU_LAPET Detected , But This Script Not Supported For Debian !"
+  exit 1
+elif [[ $OS_MU_LAPET == "ubuntu" ]]; then
+  echo "$OS_MU_LAPET Detected , But This Script Not Supported For Ubuntu !"
+  exit 1
+else
+  echo "$OS_MU_LAPET Detected , But This Script Not Supported For $OS_MU_LAPET"
+  exit 1
+fi
+
 # Installation && Updating Repository
 yum update -y
 yum upgrade -y
