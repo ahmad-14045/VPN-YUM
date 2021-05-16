e282472ca42210ffeaf853b3655322367e0ffe35
wildyproject <84273222+wildyproject@users.noreply.github.com>
2021-05-16 22:46:11 +0700

    Create about.sh

---
+++ Tools/about.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# About
+
+clear
+echo "
+===========================
+   Script Information :
+===========================
+OS Supported Only For :
+* Centos 6
+* Centos 7
+* Centos 8
+* Fedora 31
+* Fedora 32
+* Fedora 33
+===========================
+Created By WildyProject "
+echo ""
+echo "
+===========================
+       Contact Me : 
+===========================
+Telegram : @wildyproject
+Whatsapp : +6285830995000
+==========================="
