d0db3cb2dddaaa16d46f467fd700305cceccbe3c
wildyproject <84273222+wildyproject@users.noreply.github.com>
2021-05-16 21:36:48 +0700

    Create menu.sh

---
+++ Tools/menu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Menu On Script
+# ========================
+
+clear
+echo "================ Menu ================"
+echo " addssh     : Create SSH Account"
+echo " delssh     : Delete SSH Account"
+echo " sshlogin   : Check SSH User Login"
+echo " trialssh   : Generate Trial Account"
+echo " sshuser    : List Of All SSH User "
+echo "=============== System ==============="
+echo " speedtest  : Checking Network Speed "
+echo " vnstat     : Checking Bandwidth"
+echo " restart    : Restarting Service"
+echo " neofetch   : Checking System Info"
+echo " about      : About This Script "
+echo " exit       : Exit From VPS"
+echo " reboot     : Reboot VPS / System "
+echo "======================================"
+
