711e64d307609395cf62bfb6f3155b016746e36f
wildyproject <84273222+wildyproject@users.noreply.github.com>
2021-05-16 19:06:32 +0700

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-# Auto Setup SSH & OpenVPN Tunneling
+# Auto Setup SSH VPN Tunneling
 # Supported For Centos 7 , 8 & Fedora 31 , 32 , 33
-# Auther : Wildy Project ( Wildy Sheverando )
+# Auther  : Wildy Project ( Wildy Sheverando )
+# License : ©
 # ======================================================
 
-# Installation
+# Installation && Updating Repository
+yum update -y
+yum upgrade -y
+yum install curl -y
+yum install wget -y
+
